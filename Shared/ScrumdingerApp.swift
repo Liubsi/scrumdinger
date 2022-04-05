1015c158274c79a11341bf8c7e3bd0d58e2a7f5a
Alex Liu <junhual07@gmail.com>
2022-04-05 01:45:02 -0400

    Initial Commit

---
+++ Shared/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Shared
+//
+//  Created by Nimloth on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
