619e1d2276c2489e0e805fe71fa402a2c1c11f50
Charles Goell <charlesgoell@gmail.com>
2022-04-12 00:39:14 -0400

    displaying data in a list, creating a navigation hierarchy

---
+++ Shared/ScrumsView.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger (iOS)
+//
+//  Created by Charles Goell on 4/6/22.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView()
+    }
+}
