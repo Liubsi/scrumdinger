55e6aa0b1c18ce3326af35bd2ac2ea20e862bff2
Charles Goell <charlesgoell@gmail.com>
2022-04-12 01:09:50 -0400

    displaying data in a list, creating a navigation hierarchy

--- Shared/ScrumsView.swift
+++ Shared/ScrumsView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct ScrumsView: View {
+    let scrums: [DailyScrum]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                        
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar {
+            Button (action: {}) {
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
+        }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView()
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
     }
 }
